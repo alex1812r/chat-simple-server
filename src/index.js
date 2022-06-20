eaeae61f674551e7319bd503566c866711abc231
Alex Romero <alex1812r@gmail.com>
2022-06-20 19:26:44 -0400

    env port

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ app.get('/messages', async (_req, res) => {
 })
 
 const server = http.createServer(app);
-server.listen(5000, () => {
-  console.log('[SERVER]: server running on port 3000');
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`[SERVER]: server running on port ${process.env.PORT}`);
 });
 
 const io = new Server(server, { cors: {} });
